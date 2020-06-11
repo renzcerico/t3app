9bde77afbea7f59b91113e9fdf5b47be2d57372c
onlineservice <onlineservice@onlinesmrf.repository.com>
2020-06-11 09:45:29 +0800

    added accounts sequence

---
+++ SQL/Accounts/accounts_auto_inc.sql
@@ -0,0 +1,8 @@
+--------------------------------------------------------
+--  File created - Thursday-June-11-2020   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Sequence ACCOUNTS_AUTO_INC
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "T3"."ACCOUNTS_AUTO_INC"  MINVALUE 1 MAXVALUE 999999999999 INCREMENT BY 1 START WITH 41 CACHE 20 NOORDER  NOCYCLE ;
